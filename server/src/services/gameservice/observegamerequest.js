f11898daac1ba195f1f311c98a17fd266375c790
Christopher Kolb <CkAstro.git@gmail.com>
2022-06-25 19:49:42 -0400

    confirmed working with GameManager

--- server/src/services/gameservice/observegamerequest.js
+++ server/src/services/gameservice/observegamerequest.js
@@ -1,37 +1,12 @@
 import { clients } from '../../api/websocket/clients.js';
-import games from './games.js';
+import gameManager from './gamemanager.js';
+'use strict';
 
 const handleObserveGameRequest = ({ clientId, data }) => {
    const client = clients[clientId];
    const gameId = data.gameId;
 
-   // get a game pointer
-   let game;
-   for (const g of games.activeGames) {
-      if (g.gameId === gameId) {
-         game = g;
-         break;
-      }
-   }
-   
-   // if game exists add player to observation list
-   if (game) {
-      client.activeGame = game;
-      game.observers = game.observers.concat(client);
-      console.log(`client ${clientId} now observing game ${gameId}`);
-      client.send('activeGameUpdate', {
-         activeGame: true, 
-         playerColor: 'observer', 
-         opponent: `${game.black.playerId} vs ${game.white.playerId}`,
-         black: game.black.playerId,
-         white: game.white.playerId,
-      });
-
-      // wait for client page to load before sending update
-      setTimeout(() => game.sendGameUpdate(client), 100);
-   } else {
-      client.send('errorMessage', {errorText: 'Game no longer exists!'});
-   }
-};
+   gameManager.requestObserveGame(client, gameId);
+}
 
 export default handleObserveGameRequest;
